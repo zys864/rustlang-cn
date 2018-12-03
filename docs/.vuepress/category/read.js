62741286f3204b6595b1fe18b997b05257236279
krircc <krircc@aliyun.com>
2018-12-03 23:03:21 +0800

    Update read.js

--- docs/.vuepress/category/read.js
+++ docs/.vuepress/category/read.js
@@ -1,10 +1,11 @@
 exports.read = (title) => {
     return [
-        '2018/深入浅出Rust-Future-Part-1',
+        '2018/深入浅出Rust-Future-Part-2',
         {
             title: '2018',
             collapsable: true,
             children: [
+                '2018/深入浅出Rust-Future-Part-1',
                 '2018/rust-use-actix-web-build-auth-micao-serive-3',
                 '2018/Rust中的范型编程-Exonum是如何从Iron转移到Actix-web',
                 '2018/Linux高性能服务器编程模式',
