be362a4cb1b7b0109692f7c1757529d2069388e6
krircc <krircc@aliyun.com>
2018-12-03 12:00:13 +0800

    Update read.js

--- docs/.vuepress/category/read.js
+++ docs/.vuepress/category/read.js
@@ -6,7 +6,7 @@ exports.read = (title) => {
             collapsable: true,
             children: [
                 '2018/rust-use-actix-web-build-auth-micao-serive-3',
-                '2018/Rust中的范型编程-Exonum是如何从Iron转移到Actix-web',
+                '2018/Rust中范型编程-Exonum是如何从Iron转移到Actix-web',
                 '2018/Linux高性能服务器编程模式',
                 '2018/rust-use-actix-web-build-auth-micao-serive-2',
                 '2018/rust-use-actix-web-build-auth-micao-serive-1'
