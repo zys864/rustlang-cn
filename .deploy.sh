41affb04131954e70b60f18cf58f7f50bc7361fe
krircc <krircc@aliyun.com>
2019-03-29 13:17:34 +0800

    升级vuepress1.x，工作正确

--- .deploy.sh
+++ .deploy.sh
@@ -1,8 +1,7 @@
-#!/bin/bash -eu
-
-echo "Documentation uploading."
+#!/usr/bin/env sh
 
 cp CNAME docs/.vuepress/dist
-/tmp/ghp-import/ghp_import.py -n -p -f -m "Update documentation." -b master -r https://$rustlangcn@github.com/rustlang-cn/rustlang-cn.github.io.git docs/.vuepress/dist
 
-echo "Documentation uploaded."
+git clone https://github.com/davisp/ghp-import.git &&
+./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://"$rustlangcn"@github.com/rustlang-cn/rustlang-cn.github.io.git docs/.vuepress/dist &&
+echo "Uploaded documentation"
