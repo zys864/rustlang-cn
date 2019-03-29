5eb72b32a393f417dd63b132c02aa10e79ba651b
krircc <krircc@aliyun.com>
2019-03-29 15:23:50 +0800

    Update .deploy.sh

--- .deploy.sh
+++ .deploy.sh
@@ -3,5 +3,5 @@
 cp CNAME docs/.vuepress/dist
 
 git clone https://github.com/davisp/ghp-import.git &&
-./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://"$rustlangcn"@github.com/rustlang-cn/rustlang-cn.github.io.git docs/.vuepress/dist &&
+./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://$(rustlangcn)@github.com/rustlang-cn/rustlang-cn.github.io.git docs/.vuepress/dist &&
 echo "Uploaded documentation"
