c6486a817969a21998913adf67c2217bd4620ab4
krircc <krircc@aliyun.com>
2019-03-29 16:33:50 +0800

    Delete .deploy.sh

--- .deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-cp CNAME docs/.vuepress/dist
-
-git clone https://github.com/davisp/ghp-import.git &&
-./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -b master -r https://$(rustlangcn)@github.com/rustlang-cn/rustlang-cn.github.io.git docs/.vuepress/dist &&
-echo "Uploaded documentation"
